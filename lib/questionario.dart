51e398a3c5b3428f3c4898b80335ccbeb8bb36db
Richard <richard.silva@grancoffee.com.br>
2023-01-23 14:41:39 -0300

    Oitavo commit

--- lib/questionario.dart
+++ lib/questionario.dart
@@ -5,7 +5,7 @@ import './resposta.dart';
 class Questionario extends StatelessWidget {
   final List<Map<String, Object>> perguntas;
   final int perguntaSelecionada;
-  final void Function() quandoResponder;
+  final void Function(int) quandoResponder;
 
   const Questionario({
     required this.perguntas,
@@ -14,20 +14,26 @@ class Questionario extends StatelessWidget {
     super.key,
   });
 
-    bool get temPerguntaSelecionada {
+  bool get temPerguntaSelecionada {
     return perguntaSelecionada < perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    List<String> respostas = temPerguntaSelecionada
-        ? perguntas[perguntaSelecionada]['respostas'] as List<String>
+    List<Map<String, Object>> respostas = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada]['respostas']
+            as List<Map<String, Object>>
         : [];
 
     return Column(
       children: [
         Questao(perguntas[perguntaSelecionada]['texto'] as String),
-        ...respostas.map((t) => Resposta(t, quandoResponder)).toList(),
+        ...respostas.map((resp) {
+          return Resposta(
+            resp['texto'] as String,
+            () => quandoResponder(resp['pontuacao'] as int),
+          );
+        }).toList(),
       ],
     );
   }
